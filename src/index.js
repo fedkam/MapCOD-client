051cec18b4c360f04653afa8ecea51abad837683
fedkamcha <korolevfedos@yandex.ru>
2019-07-23 14:14:13 +1200

    reducer

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import * as serviceWorker from './serviceWorker';
 
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
-import reducers from './components/District/reducers';
+import reducers from './reducers';
 
 
 const store = createStore(reducers, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
