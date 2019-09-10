cfca8a75d1cf9ab98e69890eb4f7a33d7f99bff8
fed <korolevfedos@yandex.ru>
2019-09-10 17:23:25 +1200

    //

--- src/components/District/index.jsx
+++ src/components/District/index.jsx
@@ -78,7 +78,7 @@ function District(props){
     const [open, setOpen] = useState({});
 
     const handleClickCollapse = e => {
-        //console.log("____handleClickCollapse()"," event=", e," open=", open," !open=", !open[e]);
+        console.log("____handleClickCollapse()"," event=", e," open=", open," !open=", !open[e]);
         //console.log("e",e,e.length);
         if(e.length != 1){
           setOpen({[e[0]]:open[e[0]], [e[1]]:!open[e[1]]});
